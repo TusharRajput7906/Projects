c877fe91a5b88485600f19786538e638822175db
TusharRajput7906 <tusharrajput2023@gmail.com>
2026-03-26 23:23:09 +0530

    fix vite preview host

--- smart-brain-app/frontend/vite.config.js
+++ smart-brain-app/frontend/vite.config.js
@@ -1,5 +1,5 @@
-import { defineConfig } from 'vite';
-import react from '@vitejs/plugin-react';
+import { defineConfig } from 'vite'
+import react from '@vitejs/plugin-react'
 
 export default defineConfig({
   plugins: [react()],
@@ -12,4 +12,8 @@ export default defineConfig({
       },
     },
   },
-});
+  preview: {
+    host: true,
+    allowedHosts: "all"
+  }
+})
